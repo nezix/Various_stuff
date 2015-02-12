41ba523164a338bc1f87e1849fde7c187e1f84cc
nezix <nezix@users.noreply.github.com>
2015-02-12 17:07:11 +0100

    Import the python matplotlib 3D plot with legend
    Plot a 3D scatter plot with a legend for each point with python matplotlib !

---
+++ plot3D_matplotlib.py
@@ -0,0 +1,52 @@
+import pylab
+import sys
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+
+if len(sys.argv)!=2:
+  print "Usage : ",sys.argv[0]," file3dpoints.txt"
+  exit(-1)
+
+fich = open(sys.argv[1])
+lines = fich.readlines()
+fich.close()
+
+x=[]
+y=[]
+z=[]
+
+for i in lines:
+	tmp = i.split()
+	x.append(float(tmp[0]))
+	y.append(float(tmp[1]))
+	z.append(float(tmp[2]))
+
+
+fig = pylab.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+sc = ax.scatter(x,y,z)
+# now try to get the display coordinates of the first point
+
+x2s = range(len(x))
+y2s = range(len(x))
+
+for i in range(len(x)):
+	x2s[i],y2s[i],_ = proj3d.proj_transform(x[i],y[i],z[i], ax.get_proj())
+
+labels = range(len(x))
+
+for i in range(len(x)):
+	labels[i] = pylab.annotate( str(i+1), 
+	xy = (x2s[i], y2s[i]), xytext = (-1, 1),
+	textcoords = 'offset points', ha = 'right', va = 'bottom')
+
+def update_position(e):
+	for i in range(len(x)):
+		x2s[i],y2s[i],_ = proj3d.proj_transform(x[i],y[i],z[i], ax.get_proj())
+		labels[i].xy = x2s[i],y2s[i]
+		labels[i].update_positions(fig.canvas.renderer)
+	fig.canvas.draw()
+
+fig.canvas.mpl_connect('button_release_event', update_position)
+pylab.show()
